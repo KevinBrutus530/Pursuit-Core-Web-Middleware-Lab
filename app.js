9f5481574758ea5e742dd31704c2b938b4adf2b4
Kevin Brutus <kevinbrutus@pursuit.org>
2020-01-11 17:58:49 -0500

    create generateSpread function

--- app.js
+++ app.js
@@ -10,19 +10,28 @@ let animals = ["sealion", "lion", "tiger"]
 
 app.use(cors());
 
-const isAnimal = (req, res, next) => {
-    let result = req.params
-    if(animals.includes(result['animal'])){
-        res.json({status: 200, message: true})
-    } else {
-        res.json({status: 200, message: false})
-    }
-    next()
+//  ANIMAL APP
+// const isAnimal = (req, res, next) => {
+//     let result = req.params
+//     if(animals.includes(result['animal'])){
+//         res.json({status: 200, message: true})
+//     } else {
+//         res.json({status: 200, message: false})
+//     }
+//     next()
+// }
+
+// app.get("/animals/:animal", isAnimal)
+
+// RANDOM NUMBERS APP
+const generateSpread = (req, res, next) => {
+    let n1 = req.params["num1"]
+    let n2 = req.params["num2"]
+    res.json(Math.floor(Math.random() * (parseInt(n1) - parseInt(n2) + 1)) + parseInt(n2));
+    next();
 }
 
-// app.use(isAnimal);
-
-app.get("/animals/:animal", isAnimal)
+app.get("/random/:num1/:num2", generateSpread)
 
 app.listen(port, () => {
     console.log("you are running on port ", port)
