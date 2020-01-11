55c0ec5446442558a603f6948a8f7ac29b3a9477
Kevin Brutus <kevinbrutus@pursuit.org>
2020-01-11 16:27:05 -0500

    start isIncluded function

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const app = express();
 
 const port = 3000;
 
+let animals = ["sealion", "lion", "tiger"]
+
 app.use(cors());
+
+const isIncluded = (req, res, next) => {
+    if(req.params.include(animals)){
+        res.json({status: 200, message: true})
+    }
+    next()
+}
+
+app.get("/animals/:animal", isIncluded, (req, res) => {
+    // if(res.includes(animals)){
+    //     res.json({status: 200, message: true})
+    // }
+})
 
 app.listen(port, () => {
     console.log("you are running on port ", port)
