2126bf1f460d9bfcb6ac2aa10c19bae4dffe4efc
bsihame <sihamebazi@pursuit.org>
2020-01-12 11:06:25 -0500

    finished random pack

--- app.js
+++ app.js
@@ -30,8 +30,14 @@ const generateSpread = (req, res, next) => {
     res.json(Math.floor(Math.random() * (parseInt(n1) - parseInt(n2) + 1)) + parseInt(n2));
     next();
 }
+//app.use(generateSpread());
+app.get("/random/:num1/:num2",generateSpread, (req, res) =>{
+    let n1 = req.params["num1"]
+    let n2 = req.params["num2"]
+    //res.json({ status: "success", range: `[${req.params["num1"]}, ${req.params["num2"]}]`, randPick: `Math.floor(Math.random() * (parseInt(n1) - parseInt(n2) + 1)) + parseInt(n2)`})
+    res.json({ status: "success", range: `[${n1}, ${n2}]`, randPick: `${Math.floor(Math.random() * (parseInt(n1) - parseInt(n2) + 1)) + parseInt(n2)}`})
+})
 
-app.get("/random/:num1/:num2", generateSpread)
 
 app.listen(port, () => {
     console.log("you are running on port ", port)
